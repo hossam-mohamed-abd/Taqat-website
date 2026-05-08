307b8b4f977aadb843d06b59fc9641baa02da26c
Hossam Mohamed <hossam545mohamed@gmail.com>
2026-05-08 03:02:56 +0300

    first commiسt

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -4,12 +4,10 @@ export const ApiLink = "https://taqat-api-seven.vercel.app"
 export const LocalApi = "http://localhost:5000"
 
 const Api = axios.create({
-    baseURL: ApiLink ,
+    baseURL: ApiLink,
     headers: {
         'Content-Type': 'application/json',
-        // 'Accept': 'application/json',
     },
-    withCredentials: true,
 });
 
 export default Api;
